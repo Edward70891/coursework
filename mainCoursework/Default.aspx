4a7fe54c3d509d66cc51f8ad90c20780f20c6f0b
Edward DSchool <R04637@crossleyheath.org.uk>
2017-10-20 10:02:15 +0100

    Remade everything, deleted unnecessary testing stuff, started working on login first

---
+++ mainCoursework/Default.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="mainCoursework._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+		<h1>Please Login</h1>
+    </div>
+
+	<div class="login">
+		<p>User Name:</p>
+		<asp:TextBox ID="usernameBox" runat="server"></asp:TextBox>
+		<p>Password:</p>
+		<asp:TextBox ID="passwordBox" runat="server"></asp:TextBox>
+		<p></p>
+		<asp:Button ID="submitCredentialsButton" runat="server" Text="Log In" OnClick="submitCredentialsButton_Click"/>
+	</div>
+
+</asp:Content>
---
+++ mainCoursework/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Data.OleDb;
+using System.Web.UI.WebControls;
+
+namespace mainCoursework
+{
+	public partial class _Default : Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			OleDbConnection database = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=W:\Repos\mainCoursework\main.accdb");
+		}
+
+		protected void submitCredentialsButton_Click(object sender, EventArgs e)
+		{
+			attemptLogin(usernameBox.Text, passwordBox.Text);
+		}
+
+		public void attemptLogin(string username, string password)
+		{
+
+		}
+	}
+}
