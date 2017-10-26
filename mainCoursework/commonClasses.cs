2b1273644cf24aea751c448c040685c12f34bfff
Edward's Home Computer <edward7089@hotmail.co.uk>
2017-10-26 21:20:08 +0100

    Started work on logging

--- mainCoursework/commonClasses.cs
+++ mainCoursework/commonClasses.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.IO;
+using System;
+
+public static class logging
+{
+	public static void newUserEntry(string user, string entryText)
+	{
+		string writeText;
+		writeText = "[" + Convert.ToString(DateTime.Now) + "]";
+	}
+}
