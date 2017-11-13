b040830c518016f74d84ceb6ebd4d3b6ddbb0fd8
Edward's Home Computer <edward7089@hotmail.co.uk>
2017-11-13 20:30:31 +0000

    Halfway to fixing login/authentication issues

--- mainCoursework/Global.asax.cs
+++ mainCoursework/Global.asax.cs
@@ -15,7 +15,7 @@ namespace mainCoursework
 		void Application_Start(object sender, EventArgs e)
 		{
 			// Code that runs on application startup
-			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			// RouteConfig.RegisterRoutes(RouteTable.Routes);
 			BundleConfig.RegisterBundles(BundleTable.Bundles);
 			customLogging.newSession();
 		}
