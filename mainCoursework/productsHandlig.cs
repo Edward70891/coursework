cc37d9cbb897e3cad7aabfb7b9791070dd87c5f8
School Computer <R04637@crossleyheath.org.uk>
2018-01-23 15:15:24 +0000

    Switched the data handling method of the product class to a new struct called "productStruct"

--- mainCoursework/productsHandlig.cs
+++ mainCoursework/productsHandlig.cs
@@ -6,230 +6,221 @@ using System.Web;
 
 namespace mainCoursework
 {
-	public class productsHandlig
+	/// <summary>
+	/// A struct for storing product info, highly recommended to use the product class instead of this as that includes logic
+	/// </summary>
+	public struct productStruct
 	{
+		public string productName;
+		public string displayName;
+		public int stock;
+		public decimal price;
+		public string band;
+		public string description;
+		public string imagePath;
+		public string type;
+	}
+
+	/// <summary>
+	/// A class to instantiate when wanting to have an easy to manage product object with all the relevant information attached.
+	/// Please use me instead of directly accessing the database to modify stock!
+	/// </summary>
+	public class product
+	{
+		//Gets the data link
+		private defaultDataSet.productsDataTable dataTable = new defaultDataSet.productsDataTable();
+		private mainCoursework.defaultDataSetTableAdapters.productsTableAdapter adapter = new mainCoursework.defaultDataSetTableAdapters.productsTableAdapter();
+
+		//Declares the needed variables
+		private productStruct ProductInfo = new productStruct();
+		/// <summary>
+		/// All the info about the current product
+		/// </summary>
+		public productStruct productInfo
+		{
+			get
+			{
+				return ProductInfo;
+			}
+		}
+		public int newStock { get; set; }
 
 		/// <summary>
-		/// A class to instantiate when wanting to have an easy to manage product object with all the relevant information attached.
-		/// Please use me instead of directly accessing the database to modify stock!
+		/// Initialize the product using the name of a product; this method will access the database (and thus is slower)
 		/// </summary>
-		public class product
+		/// <param name="iniName">The name of the product to pull from the database</param>
+		public product(string iniName)
 		{
-			//Gets the data link
-			private defaultDataSet.productsDataTable dataTable = new defaultDataSet.productsDataTable();
-			private mainCoursework.defaultDataSetTableAdapters.productsTableAdapter adapter = new mainCoursework.defaultDataSetTableAdapters.productsTableAdapter();
+			getData(iniName);
+		}
+		/// <summary>
+		/// Initialize the product using a datarow passed to the method, does not access database
+		/// </summary>
+		/// <param name="row">The datarow containing the product information</param>
+		public product(DataRow row)
+		{
+			ProductInfo.productName = Convert.ToString(row[0]);
+			ProductInfo.displayName = Convert.ToString(row[3]);
+			ProductInfo.stock = Convert.ToInt32(row[1]);
+			ProductInfo.price = Convert.ToDecimal(row[2]);
+			ProductInfo.band = Convert.ToString(row[7]);
+			ProductInfo.description = Convert.ToString(row[8]);
+			ProductInfo.imagePath = Convert.ToString(row[6]);
+			ProductInfo.type = Convert.ToString(row[4]);
+		}
+		
+		/// <summary>
+		/// Writes the new stock value to the database
+		/// </summary>
+		public void saveStock()
+		{
+			adapter.updateStock(newStock, ProductInfo.productName);
+			ProductInfo.stock = newStock;
+			newStock = 0;
+		}
 
-			//Declares all the read only variables about the product
-			private string productName;
-			private string displayName;
-			private int stock;
-			private int NewStock;
-			public int newStock { set { NewStock = value; } }
-			private decimal price;
-			private string band;
-			private string description;
-			private string imagePath;
-			private string type;
+		/// <summary>
+		/// Refreshes the data using data from the database regardless of how the class was initialized
+		/// </summary>
+		public void refresh()
+		{
+			getData(ProductInfo.productName);
+		}
 
-			/// <summary>
-			/// Gets all the associated data in a string array;
-			/// 0 = productName, 1 = displayName, 2 = stock, 3 = Unsaved stock value, 4 = price, 5 = band, 6 = description, 7 = imagePath, 8 = type
-			/// </summary>
-			public string[] data
-			{
-				get
-				{
-					string[] result = new string[9];
-					result[0] = productName;
-					result[1] = displayName;
-					result[2] = Convert.ToString(stock);
-					result[3] = Convert.ToString(NewStock);
-					result[4] = Convert.ToString(price);
-					result[5] = band;
-					result[6] = description;
-					result[7] = imagePath;
-					result[8] = type;
-					return result;
-				}
-			}
+		/// <summary>
+		/// Load the product straight from the database
+		/// </summary>
+		/// <param name="searchName">The name of the product to fetch</param>
+		private void getData(string searchName)
+		{
+			//Gets "all" (ie, only the one) rows with the given productname
+			var rows = dataTable.Select("productName = '" + searchName + "'");
+			//Converts the rows variable to a single object
+			System.Data.DataRow row = rows[0];
+			ProductInfo.productName = searchName;
+			//Sets all the values according the the indexes of the table columns
+			ProductInfo.displayName = Convert.ToString(row[3]);
+			ProductInfo.stock = Convert.ToInt32(row[1]);
+			ProductInfo.price = Convert.ToDecimal(row[2]);
+			ProductInfo.band = Convert.ToString(row[7]);
+			ProductInfo.description = Convert.ToString(row[8]);
+			ProductInfo.imagePath = Convert.ToString(row[6]);
+			ProductInfo.type = Convert.ToString(row[4]);
+		}
+	}
 
-			/// <summary>
-			/// Initialize the product using the name of a product; this method will access the database (and thus is slower)
-			/// </summary>
-			/// <param name="iniName">The name of the product to pull from the database</param>
-			public product(string iniName)
-			{
-				getData(iniName);
-			}
-			/// <summary>
-			/// Initialize the product using a datarow passed to the method, does not access database
-			/// </summary>
-			/// <param name="row">The datarow containing the product information</param>
-			public product(DataRow row)
-			{
-				productName = Convert.ToString(row[0]);
-				displayName = Convert.ToString(row[3]);
-				stock = Convert.ToInt32(row[1]);
-				price = Convert.ToDecimal(row[2]);
-				band = Convert.ToString(row[7]);
-				description = Convert.ToString(row[8]);
-				imagePath = Convert.ToString(row[6]);
-				type = Convert.ToString(row[4]);
-			}
 
-			/// <summary>
-			/// Writes the new stock value to the database
-			/// </summary>
-			public void saveStock()
-			{
-				adapter.updateStock(NewStock, productName);
-				stock = NewStock;
-				NewStock = 0;
-			}
 
-			/// <summary>
-			/// Refreshes the data using data from the database regardless of how the class was initialized
-			/// </summary>
-			public void refresh()
-			{
-				getData(productName);
-			}
 
-			/// <summary>
-			/// Load the product straight from the database
-			/// </summary>
-			/// <param name="searchName">The name of the product to fetch</param>
-			private void getData(string searchName)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/// <summary>
+	/// A class for holding a list of products; filtering and sorting them
+	/// </summary>
+	public class productList
+	{
+		//The complete list of all the products initialised
+		private product[] masterList;
+		//The variable that all the code in this class will modify
+		private product[] WorkingList;
+		//Read only property that returns the working list for the caller to use
+		public product[] list
+		{
+			get
 			{
-				//Gets "all" (ie, only the one) rows with the given productname
-				var rows = dataTable.Select("productName = '" + searchName + "'");
-				//Converts the rows variable to a single object
-				System.Data.DataRow row = rows[0];
-				productName = searchName;
-				//Sets all the values according the the indexes of the table columns
-				displayName = Convert.ToString(row[3]);
-				stock = Convert.ToInt32(row[1]);
-				price = Convert.ToDecimal(row[2]);
-				band = Convert.ToString(row[7]);
-				description = Convert.ToString(row[8]);
-				imagePath = Convert.ToString(row[6]);
-				type = Convert.ToString(row[4]);
+				return WorkingList;
 			}
 		}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		/// <summary>
+		/// Resets the working list to be identical to the master (ie. the raw data)
+		/// </summary>
+		public void resetWorkingList()
+		{
+			WorkingList = masterList;
+		}
 
 		/// <summary>
-		/// A class for holding a list of products; filtering and sorting them
+		/// Sets the list to be the entire contents of the database table, initializes working list
 		/// </summary>
-		public class productList
+		public void importAll()
 		{
-			//The complete list of all the products initialised
-			private product[] masterList;
-			//The variable that all the code in this class will modify
-			private product[] WorkingList;
-			//Read only property that returns the working list for the caller to use
-			public product[] list
+			var data = new defaultDataSet.productsDataTable();
+			int i = 0;
+			foreach (DataRow row in data.Rows)
 			{
-				get
-				{
-					return WorkingList;
-				}
+				masterList[i] = new product(row);
+				i++;
 			}
+			WorkingList = masterList;
+		}
 
-			/// <summary>
-			/// Resets the working list to be identical to the master (ie. the raw data)
-			/// </summary>
-			public void resetWorkingList()
+		/// <summary>
+		/// Call me to initialize the master list with a given set of names
+		/// </summary>
+		/// <param name="productNames">The productName value of all the products to initialize</param>
+		public void importWithString(string[] productNames)
+		{
+			int i = 0;
+			foreach (string str in productNames)
 			{
-				WorkingList = masterList;
+				masterList[i] = new product(str);
+				i++;
 			}
+		}
 
-			/// <summary>
-			/// Sets the list to be the entire contents of the database table, initializes working list
-			/// </summary>
-			public void importAll()
+		/// <summary>
+		/// Sorts the working list according to it's parameters
+		/// </summary>
+		/// <param name="ascending">If true, list will be sorted in ascending order; If false, descending order</param>
+		/// <param name="sortType">What parameter to sort the list by, "price", "stock", "name", or "band"</param>
+		public void sort(bool ascending, string sortType)
+		{
+			switch (sortType)
 			{
-				var data = new defaultDataSet.productsDataTable();
-				int i = 0;
-				foreach (DataRow row in data.Rows)
-				{
-					masterList[i] = new product(row);
-					i++;
-				}
-				WorkingList = masterList;
+				case "price":
+
+				case "stock":
+
+				case "name":
+
+				case "band":
+
+				default:
+					throw new System.ArgumentException("The sort type must be one of the specified values.");
 			}
+		}
 
-			/// <summary>
-			/// Call me to initialize the master list with a given set of names
-			/// </summary>
-			/// <param name="productNames">The productName value of all the products to initialize</param>
-			public void importWithString(string[] productNames)
+		/// <summary>
+		/// Filters the working list
+		/// </summary>
+		/// <param name="field">What piece of data to filter by; "band", "type" or "stock" (stock is treated as all things in stock)</param>
+		/// <param name="value">What value to look for</param>
+		/// <param name="whitelist">Set me to true to keep only entries with that value; false to keep all but that value</param>
+		public void filter(string field, string value, bool whitelist)
+		{
+			switch (field)
 			{
-				int i = 0;
-				foreach (string str in productNames)
-				{
-					masterList[i] = new product(str);
-					i++;
-				}
-			}
+				case "band":
 
-			/// <summary>
-			/// Sorts the working list according to it's parameters
-			/// </summary>
-			/// <param name="ascending">If true, list will be sorted in ascending order; If false, descending order</param>
-			/// <param name="sortType">What parameter to sort the list by, "price", "stock", "name", or "band"</param>
-			public void sort(bool ascending, string sortType)
-			{
-				switch (sortType)
-				{
-					case "price":
+				case "type":
 
-					case "stock":
+				case "stock":
 
-					case "name":
-
-					case "band":
-
-					default:
-						throw new System.ArgumentException("The sort type must be one of the specified values.");
-				}
-			}
-
-			/// <summary>
-			/// Filters the working list
-			/// </summary>
-			/// <param name="field">What piece of data to filter by; "band", "type" or "stock" (stock is treated as all things in stock)</param>
-			/// <param name="value">What value to look for</param>
-			/// <param name="whitelist">Set me to true to keep only entries with that value; false to keep all but that value</param>
-			public void filter(string field, string value, bool whitelist)
-			{
-				switch (field)
-				{
-					case "band":
-
-					case "type":
-
-					case "stock":
-
-					default:
-						throw new System.ArgumentException("The filter field must be one of the specified values");
-				}
+				default:
+					throw new System.ArgumentException("The filter field must be one of the specified values");
 			}
 		}
 	}
