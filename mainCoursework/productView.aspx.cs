6fbd49a7ee2d8bf060b7b55dd428a1e77f8238fc
Edward Duffy (Github Desktop) <edward70891@gmail.com>
2018-05-05 18:27:27 +0100

    Adjustments to productView Added productView to writeup Changed colour of text in employeeBasic

--- mainCoursework/productView.aspx.cs
+++ mainCoursework/productView.aspx.cs
@@ -12,8 +12,6 @@ namespace mainCoursework
 		//Create a new product
 		product currentProduct;
 		defaultDataSetTableAdapters.cartsTableAdapter cartsAdaptor = new defaultDataSetTableAdapters.cartsTableAdapter();
-		defaultDataSetTableAdapters.productsTableAdapter productsAdaptor = new defaultDataSetTableAdapters.productsTableAdapter();
-
 		protected void Page_Load(object sender, EventArgs e)
 		{
 			//If there's no product instruction to load, redirect the user back to the product list
@@ -23,10 +21,9 @@ namespace mainCoursework
 			}
 			//Instantiate a new product and a new panel, make them, change the css class then add them
 			currentProduct = new product(Convert.ToString(Session["productRedirectName"]));
-			productPanel currentPanel = new productPanel(currentProduct.productInfo);
-			Panel toAdd = currentPanel;
-			toAdd.CssClass = "fullProductPanel";
-			product.Controls.Add(toAdd);
+			productPanel currentPanel = new productPanel(currentProduct);
+			currentPanel.CssClass = "fullProductPanel";
+			product.Controls.Add(currentPanel);
 		}
 
 		protected void cartButton_Click(object sender, EventArgs e)
@@ -35,29 +32,26 @@ namespace mainCoursework
 			if (Convert.ToString(Session["userType"]) != "customer")
 			{
 				returnLabel.Text = "You must log in to a customer account to have a cart!";
+				return;
 			}
+			//Make sure they're not trying to add 0 of something to their cart
+			if (amountToAdd.Text == "0")
+			{
+				returnLabel.Text = "You can't add 0 of something to your cart!";
+				return;
+			}
+
+			//If isn't already an entry for that product in that cart, add it
+			if (cartsAdaptor.checkExisting(Convert.ToString(Session["currentUser"]), currentProduct.productName) == null)
+			{
+				cartsAdaptor.insertProduct(Convert.ToString(Session["currentUser"]), currentProduct.productName, Convert.ToInt32(amountToAdd.Text));
+			}
+			//If there is, add the amount they want onto it
 			else
 			{
-				//Make sure they're not trying to add 0 of something to their cart
-				if (amountToAdd.Text != "0")
-				{
-					//If isn't already an entry for that product in that cart, add it
-					if (cartsAdaptor.checkExisting(Convert.ToString(Session["currentUser"]), currentProduct.productInfo.productName) == null)
-					{
-						cartsAdaptor.insertProduct(Convert.ToString(Session["currentUser"]), currentProduct.productInfo.productName, Convert.ToInt32(amountToAdd.Text));
-					}
-					//If there is, add the amount they want onto it
-					else
-					{
-						cartsAdaptor.updateAmount(Convert.ToInt32(amountToAdd.Text), Convert.ToString(Session["currentUser"]), currentProduct.productInfo.productName);
-					}
-					returnLabel.Text = amountToAdd.Text + " " + currentProduct.productInfo.productName + "s added to your cart!";
-				}
-				else
-				{
-					returnLabel.Text = "You can't add 0 of something to your cart!";
-				}
+				cartsAdaptor.updateAmount(Convert.ToInt32(amountToAdd.Text), Convert.ToString(Session["currentUser"]), currentProduct.productName);
 			}
+			returnLabel.Text = amountToAdd.Text + " " + currentProduct.productName + "s added to your cart!";
 		}
 	}
 }
